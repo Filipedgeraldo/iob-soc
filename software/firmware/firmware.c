84d447b92ed67024f44e9baaaaf6a554317b9f89
Filipedgeraldo <filipedgeraldo@gmail.com>
2020-10-11 01:34:13 +0100

    adição do timer

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -5,9 +5,25 @@
 
 int main()
 {
+  unsigned long long elapsed ;
+  unsigned int elapsedu ;
+
+  timer_reset(TIMER_BASE);
+
+  
   //init uart 
   uart_init(UART_BASE,FREQ/BAUD);   
   uart_printf("\n\n\nHello world!\n\n\n");
+  uart_txwait();
+
+  //read current time
+  elapsed = timer_get_count(TIMER_BASE);
+  elapsedu = timer_time_us(TIMER_BASE);
+  uart_printf("\nExexution time: %d clocks: %d us at %d MHz\n\n",(unsigned int) elapsed, elapsedu,FREQ/1000000);
+  uart_txwait();
+  
   //char *a = malloc(10);
   //free(a);
+
+  return 0;
 }
